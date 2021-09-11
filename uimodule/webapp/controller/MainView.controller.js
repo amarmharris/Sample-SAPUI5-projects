65a0510e1c91264d148f1de0c671aff2b20da5d0
Harris <amar.harris@capgemini.com>
2021-09-10 19:29:35 -0500

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/ct/CountdownTimer/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.ct.CountdownTimer.controller.MainView", {});
+});
